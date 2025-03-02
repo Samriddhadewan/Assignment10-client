a94cb1aa749b4a217739fd99a2b59ac9a9df61cc
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-03-02 20:31:45 +0600

    new campaign form

--- src/Components/NewCampaign.jsx
+++ src/Components/NewCampaign.jsx
@@ -1,42 +1,100 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import { AuthContext } from "../Provider/AuthProvider";
 
 const NewCampaign = () => {
+  const { user } = useContext(AuthContext);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedValue, setSelectedValue] = useState("Campaign type");
+  const [description, setDescription] = useState("");
+
+
+  const handleNewCampaign =(e)=>{
+    e.preventDefault();
+
+
+    const form = e.target;
+    const thumbnail = form.thumbnail.value;
+    const title = form.title.value;
+    const minimum = parseInt(form.minimum.value);
+
+    const newCampaign = {
+      thumbnail: thumbnail,
+      title: title,
+      campaignType:selectedValue,
+      description,
+      minimumAmount: minimum,
+      deadline:selectedDate.toISOString().split("T")[0],
+      userEmail: user.email,
+      userName: user.displayName
+    } 
+    console.log(newCampaign)
+  }
   return (
     <div>
-        <div className="min-h-[80vh] flex justify-center items-center">
+      <div className="min-h-[80vh] flex justify-center items-center">
         <div className="card bg-base-100 w-full max-w-xl p-14 shrink-0 shadow-2xl">
-          <form  className="">
-            <h1 className="text-4xl mb-4 text-center font-bold">Create New Campaign</h1>
+          <form onSubmit={handleNewCampaign} className="">
+            <h1 className="text-4xl mb-4 text-center font-bold">
+              Create New Campaign
+            </h1>
             <fieldset className="fieldset">
-              <label className="fieldset-label">Name</label>
+              <label className="fieldset-label">Thumbnail</label>
               <input
                 type="text"
-                name="name"
+                name="thumbnail"
                 className="input w-full"
-                placeholder="Enter your name"
+                placeholder="Upload thumbnail"
               />
-              <label className="fieldset-label">Email</label>
-              <input
-                type="email"
-                name="email"
-                className="input w-full"
-                placeholder="Enter your Email"
-              />
-              <label className="fieldset-label">Photo URL</label>
+              <label className="fieldset-label">Campaign Title</label>
               <input
                 type="text"
-                name="photo"
+                name="title"
                 className="input w-full"
-                placeholder="Enter Photo URL"
+                placeholder="Enter the name of Campaign"
               />
-              <label className="fieldset-label">Password</label>
+              <select
+
+                required
+                value={selectedValue}
+                onChange={(e)=> setSelectedValue(e.target.value)}
+                className="select w-full my-2"
+              >
+                <option disabled={true}>Campaign type</option>
+                <option>Personal Issue</option>
+                <option>Start Up</option>
+                <option>Business</option>
+                <option>Creative Idea</option>
+              </select>
+              <textarea
+                placeholder="description"
+                className="textarea textarea-sm w-full"
+                value={description}
+                onChange={(e)=> setDescription(e.target.value)}
+              ></textarea>
+
+              <label className="fieldset-label">Minimum Donation Amount</label>
               <input
-                type="password"
-                name="password"
+                type="number"
+                name="minimum"
                 className="input w-full"
-                placeholder="Enter Password"
+                placeholder="Enter the minimum Donation Amount"
               />
+
+              {/* pick a date section  */}
+              <div className="w-full">
+                <label className="block mb-2">Pick a Date:</label>
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  dateFormat="yyyy-MM-dd"
+                  minDate={new Date()} 
+                  placeholderText="Click to select a date"
+                  className="border border-gray-400 p-2 rounded-md w-full"
+                />
+              </div>
               <button className="btn bg-[#0E7A81] text-white mt-4">
                 Create New Campaign
               </button>
@@ -45,7 +103,7 @@ const NewCampaign = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewCampaign
+export default NewCampaign;
